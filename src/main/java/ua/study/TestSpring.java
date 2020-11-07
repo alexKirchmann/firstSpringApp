c5354fb23587ae2e4d8531f616153408abe6ef36
alexKirchmann <tauva@ukr.net>
2020-11-07 22:55:13 +0200

    Scope Singleton, Prototype

--- src/main/java/ua/study/TestSpring.java
+++ src/main/java/ua/study/TestSpring.java
@@ -6,9 +6,22 @@ public class TestSpring {
     public static void main(String[] args) {
         ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("applicationContext.xml");
 
-        MusicPlayer musicPlayer = context.getBean("musicPlayer", MusicPlayer.class);
-        musicPlayer.playMusic();
-        System.out.println(musicPlayer.getName()+"\n"+musicPlayer.getVolume());
+        MusicPlayer musicPlayer1 = context.getBean("musicPlayer", MusicPlayer.class);
+        MusicPlayer musicPlayer2 = context.getBean("musicPlayer", MusicPlayer.class);
+
+        boolean comprasion = musicPlayer1 == musicPlayer2;
+
+        System.out.println(comprasion);
+
+        System.out.println(musicPlayer1);
+        System.out.println(musicPlayer2);
+
+        musicPlayer1.setVolume(10);
+
+        System.out.println(musicPlayer1.getVolume()+"\n"+musicPlayer2.getVolume());
+
+        musicPlayer1.playMusic();
+        musicPlayer2.playMusic();
 
         context.close();
     }
