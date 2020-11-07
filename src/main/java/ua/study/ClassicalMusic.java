d33cb80ed4e226167068700b44f8969a3e6372af
alexKirchmann <tauva@ukr.net>
2020-11-07 23:24:37 +0200

    init-, destroy-, factory-method

--- src/main/java/ua/study/ClassicalMusic.java
+++ src/main/java/ua/study/ClassicalMusic.java
@@ -2,6 +2,19 @@ package ua.study;
 
 public class ClassicalMusic implements Music {
 
+    private ClassicalMusic(){}
+
+    public static ClassicalMusic getClassicalMusic() {
+        return new ClassicalMusic();
+    }
+    private void doMyInit() {
+        System.out.println("INITIALIZING");
+    }
+
+    private void doMyDestroy() {
+        System.out.println("DESTROYING");
+    }
+
     @Override
     public String getSong() {
         return "Hungarian Rhapsody";
