a534a54030cb50a211d01f749c7332219f906e26
alexKirchmann <tauva@ukr.net>
2020-11-07 22:41:32 +0200

    Dependenct Injection 2: Values, References, Lists

--- src/main/java/ua/study/MusicPlayer.java
+++ src/main/java/ua/study/MusicPlayer.java
@@ -1,13 +1,51 @@
 package ua.study;
 
-public class MusicPlayer {
-    private Music music;
+import java.util.ArrayList;
+import java.util.List;
 
-    public MusicPlayer(Music music) {
-        this.music = music;
+public class MusicPlayer {
+    private String _name;
+    private List<Music> _musicList = new ArrayList<>();
+    private int _volume;
+
+
+
+    public MusicPlayer(){}
+
+    public MusicPlayer(List<Music> musicList) {
+        _musicList = musicList;
     }
+
+    public String getName() {
+        return _name;
+    }
+
+    public List<Music> getMusicList() {
+        return _musicList;
+    }
+
+    public int getVolume() {
+        return _volume;
+    }
+
+    public void setName(String name) {
+        _name = name;
+    }
+
+    public void setMusicList(List<Music> musicList) {
+        _musicList = musicList;
+    }
+
+    public void setVolume(int volume) {
+        _volume = volume;
+    }
+
+
 
     public void playMusic() {
-        System.out.println("Playing: " + music.getSong());
+        for (int i = 0; i < _musicList.size(); i++){
+            System.out.println("Playing: " + _musicList.get(i).getSong());
+        }
     }
+
 }
--- src/main/java/ua/study/TestSpring.java
+++ src/main/java/ua/study/TestSpring.java
@@ -8,6 +8,7 @@ public class TestSpring {
 
         MusicPlayer musicPlayer = context.getBean("musicPlayer", MusicPlayer.class);
         musicPlayer.playMusic();
+        System.out.println(musicPlayer.getName()+"\n"+musicPlayer.getVolume());
 
         context.close();
     }
